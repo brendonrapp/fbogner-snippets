0c6e36719064858ca54576a86da66144f6ab77e8
Florian Bogner <florian@bogner.sh>
2015-01-20 14:42:15 +0100

    Fixed a bug that caused commands to output to a file instead of dev null

--- serverProxy.sh
+++ serverProxy.sh
@@ -13,18 +13,18 @@ DNSSD_RUNNING=1
 while [ true ]; do
 
 	echo "Pinging $SERVER_IP..."
-	ping -c 1 "$SERVER_IP" 1>2 2>/dev/null	#ping the server
+	ping -c 1 "$SERVER_IP" &>/dev/null	#ping the server
 	
 	if [ "$?" -eq "0" ]; then	#and check if it's available
 		# server reachable
 		echo "$SERVER_IP is alive"
 		
-		kill -0 "$PID" 1>2 2>/dev/null	#check if dns proxy is still running
+		kill -0 "$PID" &>/dev/null	#check if dns proxy is still running
 		DNSSD_RUNNING=$?
 		
 		if [ "$DNSSD_RUNNING" -ne "0" ]; then	# if we dont have a running dns-sd instance start it
 			echo "Starting DNS SD Proxy for service $SERVICE_TYPE on server $SERVER_NAME"
-			dns-sd -P "$SERVER_NAME" "$SERVICE_TYPE" local $SERVICE_PORT "$SERVER_NAME.local" "$SERVER_IP" 1>2 2>/dev/null &
+			dns-sd -P "$SERVER_NAME" "$SERVICE_TYPE" local $SERVICE_PORT "$SERVER_NAME.local" "$SERVER_IP" &>/dev/null &
 			PID=$!;
 		else 
 			echo "DNS SD Proxy for service $SERVICE_TYPE on server $SERVER_NAME is already running"
@@ -35,7 +35,7 @@ while [ true ]; do
 		
 		if [ ! "$PID" = "" ]; then	# if we have a running dns-sd instance stop it
 			echo "Stopping DNS SD Proxy for service $SERVICE_TYPE on server $SERVER_NAME"
-			kill "$PID" 1>2 2>/dev/null
+			kill "$PID" &>/dev/null
 			PID="";
 		else 
 			echo "DNS SD Proxy for service $SERVICE_TYPE on server $SERVER_NAME is not running"
